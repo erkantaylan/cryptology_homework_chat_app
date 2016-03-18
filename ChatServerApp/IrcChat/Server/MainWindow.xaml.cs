a331e25303452ca9f5a22503f79a9926bb72c76c
erkantaylan <etaylan13@gmail.com>
2016-03-18 02:50:02 +0200

    s

---
+++ ChatServerApp/IrcChat/Server/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using Hik.Communication.Scs.Communication.EndPoints.Tcp;
+using Hik.Communication.ScsServices.Service;
+using Hik.Samples.Scs.IrcChat.Contracts;
+
+namespace Hik.Samples.Scs.IrcChat.Server
+{
+    public partial class MainWindow : Window
+    {
+        private IScsServiceApplication _serviceApplication;
+
+        private ChatService _chatService;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            lblUsers.Text = "";
+            this.Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            Environment.Exit(1);
+        }
+
+        /// <summary>
+        /// Handles Client event of 'Start Server' button.
+        /// </summary>
+        /// <param name="sender">Source of event</param>
+        /// <param name="e">Event arguments</param>
+        private void btnStartServer_Click(object sender, RoutedEventArgs e)
+        {
+            //Get TCP port number from textbox
+            int port = Convert.ToInt32(txtPort.Value); 
+            try
+            {
+                _serviceApplication = ScsServiceBuilder.CreateService(new ScsTcpEndPoint(port));
+                _chatService = new ChatService();
+                _serviceApplication.AddService<IChatService, ChatService>(_chatService);
+                _chatService.UserListChanged += chatService_UserListChanged;
+                _serviceApplication.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Service can not be started. Exception detail: " + ex.Message);
+                return;
+            }
+            
+            txtPort.IsEnabled = false;
+        }
+
+        private void chatService_UserListChanged(object sender, EventArgs e)
+        {
+            Dispatcher.Invoke(new Action(UpdateUserList));
+        }
+
+        /// <summary>
+        /// Handles Client event of 'Stop Server' button.
+        /// </summary>
+        /// <param name="sender">Source of event</param>
+        /// <param name="e">Event arguments</param>
+        private void btnStopServer_Click(object sender, RoutedEventArgs e)
+        {
+            if (_serviceApplication == null)
+            {
+                return;
+            }
+
+            _serviceApplication.Stop();
+            
+            txtPort.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// Updates user list on GUI.
+        /// </summary>
+        private void UpdateUserList()
+        {
+            var users = new StringBuilder();
+            foreach (var user in _chatService.UserList)
+            {
+                if (users.Length > 0)
+                {
+                    users.Append(", ");
+                }
+
+                users.Append(user.Nick);
+            }
+
+            lblUsers.Text = users.ToString();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,16 @@
+- [X]  A User login with password
+- [ ]  A User can send messages to another user
+- [ ]  A user can recieve mesasges
+- [X]  Json for user infos; usernane, password
+- [X]  Min 4 users
+- [ ]  Users send messages to just 1 user, not broadcast
+
+~ Frameworks [3/3]
+  - [X] TCP Server/Client Communication and RMI Framework https://github.com/hikalkan/scs
+  - [X] A toolkit for creating Metro / Modern UI styled WPF apps. https://github.com/MahApps/MahApps.Metro
+  - [X] Json.NET is a popular high-performance JSON framework for .NET https://github.com/JamesNK/Newtonsoft.Json
+
+
+~ Others
+  [ ] move userinfo control to server/service
+  
